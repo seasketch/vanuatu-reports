1cc9beee0caa373c779d321105b17c55b996c557
Abby Vath Meyer <avmey@ucsb.edu>
2025-09-30 20:05:05 +0000

    Update population report

--- src/components/Population.tsx
+++ src/components/Population.tsx
@@ -11,12 +11,35 @@ import {
   useSketchProperties,
 } from "@seasketch/geoprocessing/client-ui";
 import {
-  GeogProp,
   ReportResult,
   metricsWithSketchId,
-  squareMeterToKilometer,
+  percentWithEdge,
+  toPercentMetric,
 } from "@seasketch/geoprocessing/client-core";
 import project from "../../project/projectClient.js";
+
+const popMap = {
+  TAFEA: 45974,
+  SHEFA: 105888,
+  PENAMA: 35633,
+  MALAMPA: 43108,
+  SANMA: 61564,
+  TORBA: 11457,
+};
+const totalPopulation = Object.values(popMap).reduce(
+  (acc, curr) => acc + curr,
+  0,
+);
+
+// Create precalcMetrics from popMap data
+const precalcMetrics = Object.entries(popMap).map(([classId, value]) => ({
+  geographyId: null,
+  metricId: "population",
+  classId: classId,
+  sketchId: null,
+  groupId: null,
+  value: value,
+}));
 
 /**
  * Population component
@@ -38,9 +61,20 @@ export const Population: React.FunctionComponent<{ printing: boolean }> = (
     <div style={{ breakInside: "avoid" }}>
       <ResultsCard title={titleLabel} functionName="population" useChildCard>
         {(data: ReportResult) => {
-          const metrics = metricsWithSketchId(
+          const percMetricIdName = `${metricGroup.metricId}Perc`;
+          const valueMetrics = metricsWithSketchId(
             data.metrics.filter((m) => m.metricId === metricGroup.metricId),
             [id],
+          );
+
+          const percentMetrics = toPercentMetric(valueMetrics, precalcMetrics, {
+            metricIdOverride: percMetricIdName,
+          });
+          const metrics = [...valueMetrics, ...percentMetrics];
+
+          const totalPop = valueMetrics.reduce(
+            (acc, curr) => acc + curr.value,
+            0,
           );
 
           return (
@@ -59,9 +93,48 @@ export const Population: React.FunctionComponent<{ printing: boolean }> = (
                   <Trans i18nKey="Population 1">
                     This area of interest is adjacent to areas with
                   </Trans>{" "}
-                  <Pill>{metrics[0].value.toLocaleString()}</Pill> {t("people")}
-                  .
+                  <Pill>{totalPop.toLocaleString()}</Pill> {t("people")}, or{" "}
+                  <Pill>~{percentWithEdge(totalPop / totalPopulation)}</Pill>{" "}
+                  {t("of Vanuatu's 2020 population")}.
                 </p>
+
+                <Collapse
+                  title={t("Show by Province")}
+                  key={props.printing + "Province Collapse"}
+                  collapsed={!props.printing}
+                >
+                  <ClassTable
+                    rows={metrics}
+                    metricGroup={metricGroup}
+                    columnConfig={[
+                      {
+                        columnLabel: t("Province"),
+                        type: "class",
+                        width: 30,
+                      },
+                      {
+                        columnLabel: "Population",
+                        type: "metricValue",
+                        metricId: metricGroup.metricId,
+                        valueFormatter: "integer",
+                        chartOptions: {
+                          showTitle: true,
+                        },
+                        width: 20,
+                      },
+                      {
+                        columnLabel: "% of Province Population",
+                        type: "metricChart",
+                        metricId: percMetricIdName,
+                        valueFormatter: "percent",
+                        chartOptions: {
+                          showTitle: true,
+                        },
+                        width: 40,
+                      },
+                    ]}
+                  />
+                </Collapse>
 
                 <Collapse
                   title={t("Learn More")}
@@ -74,7 +147,7 @@ export const Population: React.FunctionComponent<{ printing: boolean }> = (
                       Bureau of Statistics Census.
                     </p>
                     <p>
-                      📈 Report: This report colelcts areas within 1 km of the
+                      📈 Report: This report colelcts areas within 0.5 km of the
                       area of interest and sums the population of those areas.
                     </p>
                   </Trans>
--- src/functions/population.ts
+++ src/functions/population.ts
@@ -7,6 +7,7 @@ import {
   isVectorDatasource,
   getFeaturesForSketchBBoxes,
   overlapPolygonSum,
+  Feature,
 } from "@seasketch/geoprocessing";
 import project from "../../project/projectClient.js";
 import {
@@ -28,56 +29,61 @@ export async function population(
     | Sketch<Polygon | MultiPolygon>
     | SketchCollection<Polygon | MultiPolygon>,
 ): Promise<ReportResult> {
-  // Buffer the sketch polygons by .5km
-  let bufferedSketch = sketch;
-  if (isSketchCollection(sketch)) {
-    // SketchCollection
-    if (sketch.features.some((feat) => !feat.geometry)) {
-      return { metrics: [] };
-    }
-    bufferedSketch = {
-      ...sketch,
-      features: sketch.features.map((feat) => ({
-        ...feat,
-        geometry: buffer(feat.geometry as Polygon | MultiPolygon, 0.5, {
+  // Buffer the sketch by .5km
+  const bufferedSketch = (() => {
+    if (isSketchCollection(sketch)) {
+      return {
+        ...sketch,
+        features: sketch.features.map((feat) => ({
+          ...feat,
+          geometry: buffer(feat.geometry as Polygon | MultiPolygon, 0.5, {
+            units: "kilometers",
+          })!.geometry,
+        })),
+      };
+    } else {
+      return {
+        ...sketch,
+        geometry: buffer(sketch.geometry! as Polygon | MultiPolygon, 0.5, {
           units: "kilometers",
         })!.geometry,
-      })),
-    };
-  } else {
-    // Single Sketch
-    if (!sketch.geometry) {
-      return { metrics: [] };
+      };
     }
-    bufferedSketch = {
-      ...sketch,
-      geometry: buffer(sketch.geometry! as Polygon | MultiPolygon, 0.5, {
-        units: "kilometers",
-      })!.geometry,
-    };
-  }
+  })();
 
   // Calculate overlap metrics for each class in metric group
   const metricGroup = project.getMetricGroup("population");
+  const ds = project.getMetricGroupDatasource(metricGroup);
+  if (!isVectorDatasource(ds))
+    throw new Error(`Expected vector datasource for ${ds.datasourceId}`);
+  const url = project.getDatasourceUrl(ds);
+  const features = await getFeaturesForSketchBBoxes<Polygon | MultiPolygon>(
+    bufferedSketch,
+    url,
+  );
+  const classKey = project.getMetricGroupClassKey(metricGroup);
+
   const metrics = (
     await Promise.all(
       metricGroup.classes.map(async (curClass) => {
-        const ds = project.getMetricGroupDatasource(metricGroup, {
-          classId: curClass.classId,
-        });
-        if (!isVectorDatasource(ds))
-          throw new Error(`Expected vector datasource for ${ds.datasourceId}`);
-        const url = project.getDatasourceUrl(ds);
-
-        // Fetch features overlapping with sketch
-        const features = await getFeaturesForSketchBBoxes<
-          Polygon | MultiPolygon
-        >(bufferedSketch, url);
+        let finalFeatures: Feature<Polygon | MultiPolygon>[] = [];
+        if (classKey === undefined)
+          // Use all features
+          finalFeatures = features;
+        else {
+          // Filter to features that are a member of this class
+          finalFeatures = features.filter(
+            (feat) =>
+              feat.geometry &&
+              feat.properties &&
+              feat.properties[classKey] === curClass.classId,
+          );
+        }
 
         // Calculate overlap metrics
         const overlapResult = await overlapPolygonSum(
           metricGroup.metricId,
-          features,
+          finalFeatures,
           bufferedSketch,
           { sumProperty: "Population" },
         );
